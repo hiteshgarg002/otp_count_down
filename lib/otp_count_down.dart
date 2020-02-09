4e291546e27334dc1d1ef6cbf088ce51b77f1103
hiteshgarg002 <hiteshgarg02@gmail.com>
2020-02-09 22:39:43 +0530

    initial release

--- lib/otp_count_down.dart
+++ lib/otp_count_down.dart
@@ -4,15 +4,15 @@ import 'dart:async';
 
 class OTPCountDown {
   String _countDown;
+  Timer _timer;
 
-  OTPCountDown.startOTPTimer(int timeInMS, Function callBack(String countDown)) {
-    Timer.periodic(Duration(milliseconds: 1000), (Timer timer) {
+  OTPCountDown.startOTPTimer(int timeInMS, void callBack(String countDown)) {
+    _timer = Timer.periodic(Duration(milliseconds: 1000), (Timer timer) {
       timeInMS -= 1000;
       Duration duration = Duration(milliseconds: timeInMS);
 
       if (duration.inSeconds % 60 == 0) {
         _countDown = "0${duration.inMinutes}:00";
-        print("0${duration.inMinutes}:00");
       } else {
         int seconds = duration.inSeconds % 60;
         String secondsInString = seconds.toString();
@@ -20,7 +20,6 @@ class OTPCountDown {
           secondsInString = "0$seconds";
         }
         _countDown = "0${duration.inMinutes}:$secondsInString";
-        print("0${duration.inMinutes}:$secondsInString");
       }
 
       callBack(_getCountDown);
@@ -31,4 +30,10 @@ class OTPCountDown {
   }
 
   String get _getCountDown => _countDown;
+
+  void cancelTimer() {
+    if (_timer.isActive) {
+      _timer.cancel();
+    }
+  }
 }
