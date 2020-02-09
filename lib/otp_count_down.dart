517dab8191b26afbff83208c60b6db16c4e40217
hiteshgarg002 <hiteshgarg02@gmail.com>
2020-02-09 22:15:01 +0530

    initial commit

---
+++ lib/otp_count_down.dart
@@ -0,0 +1,40 @@
+library otp_count_down;
+
+import 'dart:async';
+
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+
+class OTPCountDown {
+  String _countDown;
+
+  OTPCountDown.startOTPTimer(int timeInMS, Function callBack(String countDown)) {
+    Timer.periodic(Duration(milliseconds: 1000), (Timer timer) {
+      timeInMS -= 1000;
+      Duration duration = Duration(milliseconds: timeInMS);
+
+      if (duration.inSeconds % 60 == 0) {
+        _countDown = "0${duration.inMinutes}:00";
+        print("0${duration.inMinutes}:00");
+      } else {
+        int seconds = duration.inSeconds % 60;
+        String secondsInString = seconds.toString();
+        if (seconds < 10) {
+          secondsInString = "0$seconds";
+        }
+        _countDown = "0${duration.inMinutes}:$secondsInString";
+        print("0${duration.inMinutes}:$secondsInString");
+      }
+
+      callBack(_getCountDown);
+      if (duration.inSeconds == 0) {
+        timer.cancel();
+      }
+    });
+  }
+
+  String get _getCountDown => _countDown;
+}
